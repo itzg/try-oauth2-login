dda242876917af96b4f11bb158e043bf3f869fa6
Geoff Bourne <itzgeoff@gmail.com>
2018-12-07 15:00:23 -0600

    Adding user authorities mapper for OAuth2 login processing

--- src/main/java/me/itzg/tryoauth2login/WebSecurityConfig.java
+++ src/main/java/me/itzg/tryoauth2login/WebSecurityConfig.java
@@ -3,6 +3,7 @@ package me.itzg.tryoauth2login;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import java.nio.charset.StandardCharsets;
 import java.util.Collections;
+import java.util.stream.Collectors;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Configuration;
@@ -10,10 +11,12 @@ import org.springframework.http.MediaType;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.authority.mapping.GrantedAuthoritiesMapper;
 import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
 import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
 import org.springframework.security.oauth2.client.userinfo.OAuth2UserService;
 import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.security.oauth2.core.user.OAuth2UserAuthority;
 
 @Configuration
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
@@ -35,7 +38,22 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
         .and()
           .oauth2Login()
             .userInfoEndpoint()
-            .userService(userService());
+            .userService(userService())
+            .userAuthoritiesMapper(userAuthoritiesMapper())
+    ;
+  }
+
+  private GrantedAuthoritiesMapper userAuthoritiesMapper() {
+    return authorities -> authorities.stream()
+        .map(authority -> {
+          if (authority instanceof OAuth2UserAuthority) {
+            return new OAuth2UserAuthority(
+                "ROLE_SOCIAL_USER", ((OAuth2UserAuthority) authority).getAttributes());
+          } else {
+            return authority;
+          }
+        })
+        .collect(Collectors.toList());
   }
 
   private OAuth2UserService<OAuth2UserRequest, OAuth2User> userService() {
